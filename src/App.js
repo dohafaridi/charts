c269bdd6efb447bcead2706cd5d5e30c782ec44a
dohafaridi <dohafaridi@gmail.com>
2018-12-29 22:00:30 +0100

    feat: showing up charts using victory package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const App = () => <div>Hello world</div>;
+import StockTabel from "./StockTable";
+import StockChart from "./StockChart";
+
+class App extends React.Component {
+  state = {
+    selectedStock: null
+  };
+
+  render() {
+    const { selectedStock } = this.state;
+    const { stocks } = this.props;
+
+    return (
+      <>
+        <StockTabel
+          stocks={stocks}
+          onSelect={selectedStock => this.setState({ selectedStock })}
+        />
+        {selectedStock ? (
+          <StockChart
+            stock={selectedStock}
+            onClose={() => this.setState({ selectedStock: false })}
+          />
+        ) : null}
+      </>
+    );
+  }
+}
 
 export default App;
